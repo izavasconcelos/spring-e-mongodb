d5767f66e83bb4b83e4d906f268b228cd6bfd3d5
izavasconcelos <vasconcelos.izas@gmail.com>
2020-10-22 19:38:01 -0300

    exemplo1 com spring boot

---
+++ exemplo1/src/test/java/com/izavasconcelos/spring/exemplo1/Exemplo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izavasconcelos.spring.exemplo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exemplo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
